1dea534af1b9354f225b224ed20e665bb36116d1
RidingWolf <cybercaveman@gmail.com>
2015-12-17 21:08:31 +0100

    updated express reference

--- app.js
+++ app.js
@@ -10,7 +10,5 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () { console.log('API running on port:' + app.get('port')); });
-
 module.exports = app;
